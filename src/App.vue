931035c228e2876ca97f3433b8081f0a93c0f0a8
songji1165 <songji1165@gmail.com>
2019-05-07 19:58:52 +0900

    select박스 연동

--- src/App.vue
+++ src/App.vue
@@ -60,6 +60,7 @@ li {
   left: 0;
   width: 100%;
   height: 50px;
+  line-height: 50px
 }
 /* main1 */
 .ww-main > div {
---
+++ src/components/Today.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="ww main-wrap">
+    <div class="seletBox">
+      <select name="local" id="local" v-model="selectValue">
+        <option
+          v-for="({krName, enName}) in locations"
+          :value="enName"
+          :key="enName"
+        >
+          {{ krName }}
+        </option>
+      </select>
+    </div>
+    <div class="main current">
+      <h3 class="location">{{ selectTitle }}</h3>
+      <p class="location-time">{{ date }}</p>
+      <div class="main wear">
+        <p class="wear-icon" @click="showModal = true">
+          <img src="../assets/0.png" alt="" width="300" />
+        </p>
+        <Modal class="modal" v-if="showModal" @onClose="handleCloseModal">
+          <div slot="header">
+            <h3></h3>
+          </div>
+          <div slot="body"></div>
+        </Modal>
+      </div>
+
+      <div class="main temper">
+        <!-- <ionicons name="ios-rainy"></ionicons> -->
+        <h3 class="temper-temper">
+          <i class="fas fa-cloud-rain"></i>18 &#176;
+        </h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { getWeatherAPI } from "@/api/index.js";
+  import Modal from "./Modal";
+  import locations from "@/json/location.json";
+  // import {ionicons} from '@expo/vector-icons'
+
+  export default {
+    components: { Modal },
+    data() {
+      return {
+        showModal: false,
+        locations,
+        selectValue: "seoul",
+        lat: "",
+        lon: ""
+      };
+    },
+    methods: {
+      handleCloseModal() {
+        this.showModal = false;
+      }
+      // async getWeather(lat, lon) {
+      //   getWeatherAPI.weatherAPI(lat, lon).then(res => console.log(res));
+      // }
+    },
+    computed: {
+      date() {
+        let today = new Date();
+        let dd = today.getDate();
+        let mm = today.getMonth() + 1;
+        let yyyy = today.getFullYear();
+
+        dd < 10 ? (dd = "0" + dd) : dd;
+        mm < 10 ? (dd = "0" + mm) : mm;
+
+        let minute = today.getMinutes();
+        let hour = today.getHours();
+
+        hour <= 12 ? (hour = "오후 " + hour) : (hour = "오전 " + hour);
+
+        return `${yyyy}년 ${mm}월 ${dd}일  ${hour}시 ${minute}분`;
+      },
+      selectTitle() {
+        return this.locations[this.selectValue].krName;
+      }
+    },
+    watch: {
+      selectValue(en) {
+        console.log(en);
+        // this.lat = this.locations.en.lat;
+        console.log(this.locations[en].lat);
+      }
+    },
+    mounted() {
+      // this.getWeather(this.locations.lat, this.locations.lon);
+    }
+  };
+</script>
+
+<style scoped>
+  .main-wrap {
+    height: 100vh;
+    position: relative;
+    padding-top: 60px;
+    border: 1px solid;
+  }
+  .seletBox {
+    float: right;
+    margin-right: 5%;
+  }
+  select {
+    height: 30px;
+    width: 80px;
+    border-radius: 10px;
+    padding-left: 10px;
+  }
+  .current {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .main {
+    margin: 10px 0;
+  }
+  .main h3 {
+    font-size: 1.5rem;
+  }
+  .wear {
+    position: relative;
+  }
+  .modal {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  i {
+    margin-right: 15px;
+    vertical-align: middle;
+    font-size: 0.9em;
+  }
+  .temper h3 {
+    display: inline-block;
+    border: 1px solid;
+    border-radius: 50px;
+    padding: 5px 15px;
+  }
+</style>
