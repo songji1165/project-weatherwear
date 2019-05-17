f992cac322997c836395a2bfb3dab96bf634be9b
songji1165 <songji1165@gmail.com>
2019-05-17 19:22:24 +0900

    로딩화면 수정

--- src/components/TemperInfo.vue
+++ src/components/TemperInfo.vue
@@ -2,9 +2,13 @@
   <div class="info-wrap">
     <div class="wear">
       <div>
-          <slot name="daytime-title" class="temper-info-title"> </slot>
-        <p class="wear-icon" >
-          <img :src="imageSrc" :class="{ smallIcon: isSmallIcon }" @click="handleCloseModal"/>
+        <slot name="daytime-title" class="temper-info-title"> </slot>
+        <p class="wear-icon">
+          <img
+            :src="imageSrc"
+            :class="{ smallIcon: isSmallIcon }"
+            @click="handleCloseModal"
+          />
         </p>
       </div>
       <Modal class="modal" v-if="showModal" @onClose="handleCloseModal">
@@ -20,104 +24,105 @@
     <div class="temper">
       <p class="temper-box">
         <i :class="fasIcon"></i>
-        {{ temper }} &#176 
+        {{ temper }} &deg;
       </p>
     </div>
   </div>
 </template>
 
 <script>
-  import Modal from "@/components/Modal";
-  import {
-    wearIconNum,
-    selectedTempScope,
-    selectedClothes,
-    weatherIconSelect
-  } from "@/modules/search.js";
+import Modal from "@/components/Modal";
+import {
+  wearIconNum,
+  selectedTempScope,
+  selectedClothes,
+  weatherIconSelect
+} from "@/modules/search.js";
 
-  export default {
-    components: { Modal },
-    props: ["temper", "description", "isSmallIcon"],
-    watch: {
-      temper(temper) {
-        this.imageWearNum = wearIconNum(temper);
-      },
-      description(description) {
-        this.fasIcon = weatherIconSelect(description).iconName;
-      }
+export default {
+  components: { Modal },
+  props: ["temper", "description", "isSmallIcon"],
+  watch: {
+    temper(temper) {
+      this.imageWearNum = wearIconNum(temper);
     },
-    data() {
-      return {
-        showModal: false,
-        fasIcon: "",
-        imageWearNum: "0",
-        showTitle: false
-      };
+    description(description) {
+      this.fasIcon = weatherIconSelect(description).iconName;
+      console.log('2번 info전달',description)
+      this.$emit("changeIcon");
+    }
+  },
+  data() {
+    return {
+      showModal: false,
+      fasIcon: "",
+      imageWearNum: "0",
+      showTitle: false
+    };
+  },
+  methods: {
+    handleCloseModal() {
+      this.showModal = !this.showModal;
+    }
+  },
+  computed: {
+    imageSrc() {
+      return require(`@/assets/${this.imageWearNum}.png`);
     },
-    methods: {
-      handleCloseModal() {
-        this.showModal = !this.showModal;
-      }
+    currentTemperScope() {
+      return selectedTempScope(this.imageWearNum);
     },
-    computed: {
-      imageSrc() {
-        return require(`@/assets/${this.imageWearNum}.png`)
-        
-      },
-      currentTemperScope() {
-        return selectedTempScope(this.imageWearNum);
-      },
-      currentClothes() {
-        return selectedClothes(this.imageWearNum);
-      }
-    },
-    // mounted(){
-    //   imageSrc()
-    // }
-  };
+    currentClothes() {
+      return selectedClothes(this.imageWearNum);
+    }
+  }
+  // mounted(){
+  //   imageSrc()
+  // }
+};
 </script>
 
 <style scoped>
-  .info-wrap {
-    padding: 10px 0
-  }
-  .wear {
-    position: relative;
-    margin-bottom: 30px;
-    height: 100%;
-    width: 100%;
-    margin: 0 auto;
-    display: table;
-  }
-  .wear > div {
-    display: table-cell;
-    vertical-align: middle;
-  }
-  img {
-    vertical-align: middle;
-    width: 300px;
-  }
-  .smallIcon {
-    width: 200px;
-  }
-  .modal {
-    position: absolute;
-    top:50%;
-    left: 50%;
-    width: 100%;
-    height: 100%;
-    transform: translate(-50%,-50%)
-  }
-  i {
-    margin-right: 15px;
-    vertical-align: middle;
-  }
+.info-wrap {
+  padding: 10px 0;
+}
+.wear {
+  position: relative;
+  margin-bottom: 30px;
+  height: 100%;
+  width: 100%;
+  margin: 0 auto;
+  display: table;
+}
+.wear > div {
+  display: table-cell;
+  vertical-align: middle;
+}
+img {
+  vertical-align: middle;
+  width: 300px;
+}
+.smallIcon {
+  width: 200px;
+}
+.modal {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  height: 100%;
+  transform: translate(-50%, -50%);
+}
+i {
+  margin-right: 15px;
+  vertical-align: middle;
+}
 
-  .temper-box {
-    margin: 20px auto;
-    border: 1px solid;
-    border-radius: 50px;
-    padding: 10px 0;
-    width: 180px;
-  }
+.temper-box {
+  margin: 20px auto;
+  border: 1px solid;
+  border-radius: 50px;
+  padding: 10px 0;
+  width: 180px;
+}
 </style>
--- src/components/TemperSlider.vue
+++ src/components/TemperSlider.vue
@@ -10,7 +10,12 @@
       @mousemove="mouseMove"
     >
       <div class="current-temp">
-        <temper-info :temper="temper" :description="description"> </temper-info>
+        <temper-info
+          :temper="temper"
+          :description="description"
+          @changeIcon="changeIcon"
+        >
+        </temper-info>
       </div>
       <div class="daytime-temp">
         <div class="min-temp">
@@ -18,6 +23,7 @@
             :temper="min"
             :description="description"
             :isSmallIcon="isSmallIcon"
+            @changeIcon="changeIcon"
           >
             <div slot="daytime-title" class="daytime-title">
               최저기온
@@ -29,6 +35,7 @@
             :temper="max"
             :description="description"
             :isSmallIcon="isSmallIcon"
+            @changeIcon="changeIcon"
           >
             <div slot="daytime-title" class="daytime-title">
               최고 기온
@@ -41,129 +48,130 @@
 </template>
 
 <script>
-import { getWeatherAPI } from "@/api/index.js";
-import TemperInfo from "@/components/TemperInfo";
-// import Loading from "@/components/Loading";
+  import { getWeatherAPI } from "@/api/index.js";
+  import TemperInfo from "@/components/TemperInfo";
 
-export default {
-  components: { TemperInfo },
-  props: ["lat", "lon"],
-  watch: {
-    lat(newV, oldV) {
-      this.requestWeather(this.lat, this.lon);
-    },
-    lon(newV, oldV) {
-      this.requestWeather(this.lat, this.lon);
-    }
-  },
-  data() {
-    return {
-      temper: "",
-      max: "",
-      min: "",
-      description: "",
-      isSmallIcon: true,
-      isActive: false,
-      isDown: false,
-      startX: null,
-      scrollLeft: null
-    };
-  },
-  methods: {
-    async requestWeather(lat, lon) {
-      try {
-        const response = await getWeatherAPI(lat, lon);
-        const responseSucc = await response.data;
-        this.temper = parseInt(responseSucc.main.temp);
-        this.max = parseInt(responseSucc.main.temp_max);
-        this.min = parseInt(responseSucc.main.temp_min);
-        this.description = responseSucc.weather[0].main;
-        this.$emit("loadSucc");
-      } catch (err) {
-        return this.$router.push("/err");
+  export default {
+    components: { TemperInfo },
+    props: ["lat", "lon"],
+    watch: {
+      lat() {
+        this.requestWeather(this.lat, this.lon);
+      },
+      lon() {
+        this.requestWeather(this.lat, this.lon);
       }
     },
-    mouseDown(e) {
-      this.isDown = true;
-      this.isActive = true;
-      this.startX = e.pageX - this.$refs.slider.offsetLeft;
-      this.scrollLeft = this.$refs.slider.scrollLeft;
-      this.$emit("activeClick");
+    data() {
+      return {
+        temper: "",
+        max: "",
+        min: "",
+        description: "",
+        isSmallIcon: true,
+        isActive: false,
+        isDown: false,
+        startX: null,
+        scrollLeft: null
+      };
     },
-    mouseLeave() {
-      this.isDown = false;
-      this.isActive = false;
-    },
-    mouseUp() {
-      this.isDown = false;
-      this.isActive = false;
-    },
-    mouseMove(e) {
-      if (!this.isDown) return;
-      e.preventDefault();
-      const x = e.pageX - this.$refs.slider.offsetLeft;
-      const walk = x - this.startX;
-      if (walk <= 0) {
-        this.$refs.slider.style.left = "-100%";
-      } else {
-        this.$refs.slider.style.left = "0";
+    methods: {
+      async requestWeather(lat, lon) {
+        try {
+          const response = await getWeatherAPI(lat, lon);
+          console.log(response)
+          const { main, weather } = await response.data;
+          this.temper = parseInt(main.temp);
+          this.max = parseInt(main.temp_max);
+          this.min = parseInt(main.temp_min);
+          this.description = weather[0].main;
+          console.log("1번 description", this.description);
+        } catch (err) {
+          return this.$router.push("/err");
+        }
+      },
+      mouseDown(e) {
+        this.isDown = true;
+        this.isActive = true;
+        this.startX = e.pageX - this.$refs.slider.offsetLeft;
+        this.scrollLeft = this.$refs.slider.scrollLeft;
+        this.$emit("activeClick");
+      },
+      mouseLeave() {
+        this.isDown = false;
+        this.isActive = false;
+      },
+      mouseUp() {
+        this.isDown = false;
+        this.isActive = false;
+      },
+      mouseMove(e) {
+        if (!this.isDown) return;
+        e.preventDefault();
+        const x = e.pageX - this.$refs.slider.offsetLeft;
+        const walk = x - this.startX;
+        if (walk <= 0) {
+          this.$refs.slider.style.left = "-100%";
+        } else {
+          this.$refs.slider.style.left = "0";
+        }
+      },
+      changeIcon() {
+        console.log("3번 changeIcon");
+        this.$emit("loadSucc")
       }
     },
-    succLoad() {
-      this.showLoading = false;
+    Updated() {
+      this.mouseDown();
+      this.mouseLeave();
+      this.mouseUp();
+      this.mouseMove();
     }
-  },
-  Updated() {
-    this.mouseDown();
-    this.mouseLeave();
-    this.mouseUp();
-    this.mouseMove();
-  }
-};
+  };
 </script>
 
 <style scoped>
-.slider {
-  width: 100%;
-  overflow: hidden;
-  position: relative;
-}
-.loading {
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-.slide-wrap {
-  position: relative;
-  width: 200%;
-  top: 0;
-  transition: all 0.5s;
-  cursor: pointer;
-}
-.slide-wrap > div {
-  float: left;
-  width: 50%;
-  height: 380px;
-}
-.slide-wrap::after {
-  content: "";
-  display: block;
-  clear: both;
-}
-.daytime-temp {
-  position: relative;
-}
-.daytime-temp > div {
-  float: left;
-  width: 50%;
-}
-.daytime-title {
-  font-size: 1.5rem;
-  font-weight: bold;
-  margin-bottom: 15px;
-}
+  .slider {
+    width: 100%;
+    overflow: hidden;
+    position: relative;
+  }
+  .loading {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .slide-wrap {
+    position: relative;
+    width: 200%;
+    top: 0;
+    transition: all 0.5s;
+    cursor: pointer;
+  }
+  .slide-wrap > div {
+    float: left;
+    width: 50%;
+    height: 380px;
+  }
+  .slide-wrap::after {
+    content: "";
+    display: block;
+    clear: both;
+  }
+  .daytime-temp {
+    position: relative;
+  }
+  .daytime-temp > div {
+    float: left;
+    width: 50%;
+  }
+  .daytime-title {
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin-bottom: 15px;
+  }
 
-.active {
-  cursor: grabbing;
-}
+  .active {
+    cursor: grabbing;
+  }
 </style>
